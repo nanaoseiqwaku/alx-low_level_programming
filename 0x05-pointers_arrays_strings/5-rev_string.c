9f31d1b2d4e1327b48967ed7a57bebfd76a35bf8
nanaoseiqwaku <andrewsdiosty@gmail.com>
2022-12-20 08:17:31 -0800

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,22 +2,25 @@
 
 /**
  * rev_string - reverses a string
- * @s: input
- * Return: string in reverse
+ * @s: parameter s
  */
 
 void rev_string(char *s)
 {
-	char rv = s[0];
-	int c = 0;
-	int i;
-	
-	while (s[c] != '\0')
-		c++;
-	for (i = 0; i < c; i++)
+	char tmp;
+	int i, length1, length2;
+
+	length1 = 0;
+	length2 = 0;
+
+	while (s[length1] != '\0')
+		length1++;
+
+	length2 = length1 - 1
+	for (i = 0; i < length1 / 2; i++)
 	{
-		c--;
-		rv = s[i];
-		s[i] = s[c];
-		s[c] = rv;
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = tmp;
+		length2 -= 1;
 	}
